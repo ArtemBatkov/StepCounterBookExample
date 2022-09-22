7d1334154add7f29551187a0c158a2a104adeb29
ArtemBatkov <artembatkov2015@gmail.com>
2022-09-22 14:27:55 +0300

    Step counter from the book

--- app/src/main/java/hardroid/healthcare/Coordinator.java
+++ app/src/main/java/hardroid/healthcare/Coordinator.java
@@ -27,132 +27,111 @@ import java.util.List;
 import kotlin.jvm.internal.markers.KMutableList;
 
 
-public class Coordinator extends AppCompatActivity {
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.hardware.SensorManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.content.Context;
+import android.widget.Toast;
 
-    private SensorManager sManager;
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import kotlin.jvm.internal.markers.KMutableList;
+
+
+public class Coordinator extends AppCompatActivity
+        implements SensorEventListener {
+
+    /*
+    We have to register the listener in the onResume() method
+    and unregister it in the onPause() method. It is a good practice!
+
+    If we want to use as a background app, first the permission is required,
+    second unregistering is not necessary.
+     */
+
+
+
+    private SensorManager mSensorManager;
+    private  Sensor mSensor;
+    private boolean isSensorPresented;
     private TextView TestSensor;
     private Button TestButton;
-    private   int foot  =0 ;
-    private boolean running = false;
-    private  Sensor sensor;
-    private  int TotalSteps = 0;
-    private  int InitialTotalSteps = 0;
-    private SensorEventListener sListener;
-    private boolean initialization = true;
-
-    private boolean HasFitPermission(){
-        return ActivityCompat.checkSelfPermission(this,Manifest.permission.ACTIVITY_RECOGNITION) == PackageManager.PERMISSION_GRANTED;
-    }
-
-    private void RequestFitPermissions(){
-        List <String> permissionsToRequest = new  <String> ArrayList();
-        if(!HasFitPermission()){
-            permissionsToRequest.add(Manifest.permission.ACTIVITY_RECOGNITION);
-        }
-        if(!permissionsToRequest.isEmpty()) {
-            ActivityCompat.requestPermissions(this,    permissionsToRequest.toArray(new String[0]), 0);
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if(requestCode == 0 && grantResults.length>0){
-            for(int i = 0 ; i < grantResults.length; i++){
-                if(grantResults[i] == PackageManager.PERMISSION_GRANTED){
-                    Log.d("PermissionRequest ", permissions[i] + " granted");
-                }
-            }
-        }
-    }
+    private double mStepsSinceReboot;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.coordinator);
-
-        //Permissions
-        RequestFitPermissions();
-
-
         TestSensor = (TextView) findViewById(R.id.test_view);
         TestButton = findViewById(R.id.btn_test);
 
-
-        sManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        //SensorManager lets you access the device's sensors
-        //getSystemService -- receive system services. System Services like
-        //Gods for various apps. Most functions of apps must be rely for System Services.
-
-        sensor = sManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);//TYPE_STEP_COUNTER
-        //This method is used to get the default sensor for a given type //1234
-
-        if(sManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER) == null){
-            Toast.makeText(this, "NO STEP SENSOR",Toast.LENGTH_SHORT);
+        mSensorManager = (SensorManager) this.getSystemService(Context.SENSOR_SERVICE);
+        if(mSensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)!=null){
+            mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+            isSensorPresented = true;
         }
         else{
-//            System.out.println("INDEX: "+ sManager.getSensorList(Sensor.TYPE_STEP_COUNTER).get(0));//.getClass().getName()
-            System.out.println("You have the following step counter: "
-                    +"\nname: " + sManager.getSensorList(Sensor.TYPE_STEP_COUNTER).get(0).getName()
-                    +"\nvendor: " + sManager.getSensorList(Sensor.TYPE_STEP_COUNTER).get(0).getVendor()
-                    +"\ntype: " + sManager.getSensorList(Sensor.TYPE_STEP_COUNTER).get(0).getType()
-                    +"\nversion: " + sManager.getSensorList(Sensor.TYPE_STEP_COUNTER).get(0).getVersion()
-                    +"\npower: " + sManager.getSensorList(Sensor.TYPE_STEP_COUNTER).get(0).getPower());
-            for(int i = 0 ; i<5;i++){
-                Toast.makeText(this, "You have the following step counter: "
-                                +"\nname: " + sManager.getSensorList(Sensor.TYPE_STEP_COUNTER).get(0).getName()
-                                +"\nvendor: " + sManager.getSensorList(Sensor.TYPE_STEP_COUNTER).get(0).getVendor()
-                                +"\ntype: " + sManager.getSensorList(Sensor.TYPE_STEP_COUNTER).get(0).getType()
-                                +"\nversion: " + sManager.getSensorList(Sensor.TYPE_STEP_COUNTER).get(0).getVersion()
-                                +"\npower: " + sManager.getSensorList(Sensor.TYPE_STEP_COUNTER).get(0).getPower()
-                        ,Toast.LENGTH_LONG).show();
-            }
+            isSensorPresented = false;
         }
 
+        /*
+        1) getSystemService -- Return the handle to a system-level service by name.
+
+        2) mSensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER) -- returns the list (information) about the step counter sensor
+        */
+    }
 
 
+    @Override
+    public void onResume(){
+        super.onResume();
+        if(isSensorPresented){
+            mSensorManager.registerListener(this, mSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+    }
 
-//        if(sensor == null ){
-//            Toast.makeText(this,"No Step Sensor Detected!",Toast.LENGTH_SHORT).show() ;
-//        }else{
-//            sManager.registerListener(sListener,sensor,SensorManager.SENSOR_DELAY_UI);
-//        }
+    @Override
+    public void onPause(){
+        super.onPause();
+        if(isSensorPresented){
+            mSensorManager.unregisterListener((SensorEventListener) this);
+        }
+    }
 
-        float[] abc = new float[3];
+    @Override
+    public void onSensorChanged(SensorEvent event){
+        mStepsSinceReboot = event.values[0];
+        TestSensor.setText("Steps since reboot: \n"+mStepsSinceReboot);
 
-//        sManager.registerListener(sListener,sensor,SensorManager.SENSOR_DELAY_FASTEST);
-        sManager.registerListener(sListener,sensor,SensorManager.SENSOR_DELAY_UI);
+    }
 
-
-        TestButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                TestSensor.setText(abc[0]+" "+ abc[1]+ " "+abc[2]+" ");
-                TestSensor.setText(String.valueOf(foot));
-            }
-        });
-
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
     }
 
 
     @Override
-    protected void onResume(){
-        super.onResume();
-        running = true;
-        sensor = sManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
-
-        if(sensor == null ){
-            Toast.makeText(this,"No Step Sensor Detected!",Toast.LENGTH_SHORT).show() ;
-        }else{
-            sManager.registerListener(sListener,sensor,SensorManager.SENSOR_DELAY_UI);
-        }
-
-
+    public void onPointerCaptureChanged(boolean hasCapture) {
+        super.onPointerCaptureChanged(hasCapture);
     }
-
-
-
-
 }
+
+
 
